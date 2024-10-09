e3b1eea8db095ed968ed15d00149b7e907f60e46
saikumar rampelli <reachsai8919@gmail.com>
2024-10-09 12:47:52 +0530

    Updated the new codes

--- deploye.sh
+++ deploye.sh
@@ -7,3 +7,6 @@ Consistency is the key
 make sure you have to follow it.
 
 Hey
+
+you are missing the concentration please focus on the things which 
+makes you feel better and happy 
--- sairam.sh
+++ sairam.sh
@@ -1,3 +1,5 @@
 consistency is the altimate way to create wonders.
 
 Confidence is the key
+
+Prepare one time table and follow it accordingly.
