52fb746835746dcd5dcaa2d1b89d6b249cd0b1bb
saikumar rampelli <reachsai8919@gmail.com>
2024-10-12 16:30:49 +0530

    new code added

--- sairam.sh
+++ sairam.sh
@@ -3,3 +3,6 @@ consistency is the altimate way to create wonders.
 Confidence is the key
 
 Prepare one time table and follow it accordingly.
+
+Real time knowledge is important so be focus on what if it is in the realtime
+scinario.
